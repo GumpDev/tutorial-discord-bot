fa4609495ca816725dcd8e4a6f9d39ec3b512eae
GumpFlash <gump.flash@gmail.com>
2020-02-09 22:58:55 -0200

    ep3

---
+++ commands/aviso.js
@@ -0,0 +1,11 @@
+const config = require("../config.json");
+
+module.exports = async (client,msg) =>{
+    const avisoChannel = await msg.guild.channels.find(channel=>channel.id == config.avisosChannelId);
+    
+    var message = msg.content.split(" ");
+    message.splice(0,1);
+    message = message.join(" ");
+    await avisoChannel.send(`@everyone ${message}`);
+    msg.reply(`Avisado no canal ${avisoChannel}`);
+}
--- core.js
+++ core.js
@@ -1,19 +1,20 @@
-const Discord = require("discord.js");
-const client  = new Discord.Client();
+const Discord   = require("discord.js");
+const client    = new Discord.Client();
 
-const prefix   = "!";
-const commands = require("./scripts/commandsReader")(prefix);
+const config    = require("./config.json");
+const commands  = require("./scripts/commandsReader")(config.prefix);
 
 client.on("ready",()=>{
     console.log(`Logando com o bot ${client.user.tag}`);
 });
 
+
 client.on("message",(msg)=>{
-    if(!msg.author.bot){
-        console.log(`${msg.author.username}: ${msg.content}`);
+    if(!msg.author.bot && msg.guild){
+        if(config.debug) console.log(`${msg.author.username}: ${msg.content}`);
         const args = msg.content.split(" ");
         if(commands[args[0]]) commands[args[0]](client,msg);
     }
 });
 
-client.login("Njc2MjMyNzU4MDExNDk0NDA1.XkCycQ.E3WOYJDuusqCUaeHqa1xttBybHQ");
+client.login(config.token);
