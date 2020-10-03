38467aaa9ba927f262b494f4772af820711ee78c
Appocryphos666 <72224895+Appocryphos666@users.noreply.github.com>
2020-10-03 12:46:18 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,4 +1,5 @@
 module.exports = async (client,msg) =>{
+     if(isNaN(args[0])) return message.channel.send("Você precisa usar números válidos!")
     const channel   = msg.channel;
     const FetchMsg  = await channel.messages.fetch();
     await channel.bulkDelete(FetchMsg,true);
